36596a4f6c5d6158fcd836645f30c98d0b852536
jikSAN <lime9churr@proton.me>
2023-01-03 05:10:20 +0530

    Added functionality to create and show notes

--- database/migrations/2023_01_01_062233_create_notes_table.php
+++ database/migrations/2023_01_01_062233_create_notes_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('notes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained();
+            $table->string('title');
+            $table->longText('text');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
         });
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('notes');
     }
 };
